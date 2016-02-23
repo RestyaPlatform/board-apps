fab443b8ee4cdaa25bb802638442db8158555908
Sivachidambaram. S <s.sivachidambaram@agriya.in>
2016-02-23 11:21:12 +0530

    Jshint issue fixed

--- r_zapier/js/app.js
+++ r_zapier/js/app.js
@@ -31,8 +31,8 @@
     }
 
     function PopupCenter(url, title, w, h) {
-        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+        var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 
         var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
         var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
