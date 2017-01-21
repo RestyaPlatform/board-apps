d9a7c95ef861d8ef3dd6829df1a9e25e94eb37be
Marikannan L <marikannanl308@gmail.com>
2017-01-21 17:16:20 +0530

    Apps content updated

--- build/apps.php
+++ build/apps.php
@@ -2,13 +2,13 @@
 /**
  * Task for generating JSON and readme content for apps
  */
-$files = array(); 
+$files = array();
 function _traverse_directory($dir, $dir_count)
 {
     global $files;
     $handle = opendir($dir);
     while (false !== ($readdir = readdir($handle))) {
-        if ($readdir != '.' && $readdir != '..' && $readdir != 'node_modules' && $readdir != 'build'&& $readdir != '.git') {
+        if ($readdir != '.' && $readdir != '..' && $readdir != 'node_modules' && $readdir != 'build' && $readdir != '.git') {
             $path = $dir . '/' . $readdir;
             if (is_dir($path)) {
                 ++$dir_count;
@@ -29,7 +29,14 @@ foreach ($files as $file) {
     $folder = explode('/', $file);
     $app = json_decode(file_get_contents($file) , true);
     $apps_arr[$folder[count($folder) - 2]] = json_decode(file_get_contents($file) , true);
-    $readme.= '|[' . $app['name'] . '](' . $folder[count($folder) - 2] . ')|' . $app['description'] . '|[Restya](http://restya.com/) |' . $app['version'] . '|' . (($app['price'] == 0) ? "Free" : $app['price']) . '|[Download](https://github.com/RestyaPlatform/board-apps/releases/download/v1/' . $folder[count($folder) - 2] . '-v' . $app['version'] . '.zip)|[Repository](' . $folder[count($folder) - 2] . ')|';
+    if ($app['price'] == "Free") {
+        $download = '[Download](https://github.com/RestyaPlatform/board-apps/releases/download/v1/' . $folder[count($folder) - 2] . '-v' . $app['version'] . '.zip)';
+        $app_name = '[' . $app['name'] . '](' . $folder[count($folder) - 2] . ')';
+    } else {
+        $download = '[Visit](http://restya.com/board/apps/' . $app['id'] . ')';
+        $app_name = $app['name'];
+    }
+    $readme.= '|' . $app_name . '|' . $app['description'] . '|[Restya](http://restya.com/) |' . $app['version'] . '|' . $app['price'] . '|' . $download . '|';
     $readme.= "\n";
 }
 file_put_contents('apps.json', json_encode($apps_arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
