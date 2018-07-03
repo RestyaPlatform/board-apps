af1e2fdd79d511016b35d72c924c12b539ca8714
saravanan <s.saravanan@agriya.in>
2018-07-03 17:05:37 +0530

    Changes #115 : Code name version integration going

--- r_codename/js/app.js
+++ r_codename/js/app.js
@@ -3,10 +3,11 @@
     addRequestCallback(function(xhr, arguments) {
         
         if (arguments && arguments[0]) {
-            if (arguments && arguments[0]) {
+            if (arguments[0].indexOf('template') > -1 &&  arguments[0].indexOf('board_visibility') > -1) {
                 var arr = JSON.parse(arguments[0]);
                 arr.background_picture_url = 'https://farm2.static.flickr.com/1525/26135249912_61ee0ae6e9_b.jpg';
-            }
+                return arr;
+            }   
         }
 
     });
