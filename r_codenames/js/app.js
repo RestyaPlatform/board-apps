3ec89d05254732836c01397acc3cf4515a2a9074
saravanan <s.saravanan@agriya.in>
2018-07-04 11:01:10 +0530

    Fixes #115 : Codenames Integration issue fixed

--- r_codenames/js/app.js
+++ r_codenames/js/app.js
@@ -1,15 +1,9 @@
 (function() {
-
     addRequestCallback(function(xhr, arguments) {
-        
-        if (arguments && arguments[0]) {
-            if (arguments[0].indexOf('template') > -1 &&  arguments[0].indexOf('board_visibility') > -1) {
-                var arr = JSON.parse(arguments[0]);
-                arr.background_picture_url = 'https://farm2.static.flickr.com/1525/26135249912_61ee0ae6e9_b.jpg';
-                return arr;
-            }   
+        if (arguments && arguments[0] && arguments[0].indexOf('template') > -1) {
+            var arr = JSON.parse(arguments[0]);
+            arr.background_picture_url = 'https://farm2.static.flickr.com/1525/26135249912_61ee0ae6e9_b.jpg';
+            arguments[0] = JSON.stringify(arr);
         }
-
     });
-
 })();
