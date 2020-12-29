c01e8f662a0afb3aeca4e774150ce9277d4841ee
S SARAVANAN <s.saravanan@agriya.in>
2020-12-29 14:52:45 +0530

    Fixes #162: Restyaboard Gmail addon changes added

--- r_gmail_addon/js/app.js
+++ r_gmail_addon/js/app.js
@@ -7,7 +7,7 @@
         function insertAppAction(e) {
             if ($('#r_gmail_addon_modal').find('.modal-footer').length > 0) {
                 if ($('#r_gmail_addon_modal .modal-footer').find('#js-gmail_addon-contact').length === 0) {
-                    $('#r_gmail_addon_modal').find('.modal-footer').append('<a id="js-gmail_addon-contact" href="http://restya.com/contact?utm_source=restyaboard&utm_medium=apppopup&utm_campaign=rb-app-gmail-addon-v0.1.1' + '" target="_blank" title="' + i18next.t('Contact Us') + '" class="btn btn-primary">' + i18next.t('Contact Us') + '</a>');
+                    $('#r_gmail_addon_modal').find('.modal-footer').append('<a id="js-gmail_addon-contact" href="https://restya.com/board/apps/r_gmail_addon' + '" target="_blank" title="' + i18next.t('Visit Gmail Add-on') + '" class="btn btn-primary">' + i18next.t('Visit Gmail Add-on') + '</a>');
                 }
             }
         }
